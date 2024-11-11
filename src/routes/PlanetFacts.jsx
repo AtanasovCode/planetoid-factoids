ee86a0438995f52b57a60cf59385225da6c15da6
AtanasovCode <a_ljupce@yahoo.com>
2024-11-11 15:58:10 +0100

    added max width for larger screens on page content

--- src/routes/PlanetFacts.jsx
+++ src/routes/PlanetFacts.jsx
@@ -29,32 +29,34 @@ const PlanetFacts = ({
 
     return (
         <Container>
-            <PageContent>
-                <Nav
-                    selectedPlanet={selectedPlanet}
-                    setSelectedPlanet={setSelectedPlanet}
-                />
-                <Wrapper>
-                    <PlanetInfo
-                        filter={filter}
-                        setFilter={setFilter}
-                        name={name}
-                        image={image}
-                        planetImage={planetImage}
-                        description={description}
-                        wikipedia={wikipedia}
+            <FullPageWrapper>
+                <PageContent>
+                    <Nav
                         selectedPlanet={selectedPlanet}
+                        setSelectedPlanet={setSelectedPlanet}
+                    />
+                    <Wrapper>
+                        <PlanetInfo
+                            filter={filter}
+                            setFilter={setFilter}
+                            name={name}
+                            image={image}
+                            planetImage={planetImage}
+                            description={description}
+                            wikipedia={wikipedia}
+                            selectedPlanet={selectedPlanet}
+                        />
+                    </Wrapper>
+                </PageContent>
+                <Wrapper>
+                    <PlanetStats
+                        rotation={rotation}
+                        revolution={revolution}
+                        temp={temp}
+                        radius={radius}
                     />
                 </Wrapper>
-            </PageContent>
-            <Wrapper>
-                <PlanetStats
-                    rotation={rotation}
-                    revolution={revolution}
-                    temp={temp}
-                    radius={radius}
-                />
-            </Wrapper>
+            </FullPageWrapper>
         </Container>
     );
 }
@@ -63,13 +65,20 @@ export default PlanetFacts;
 
 const Container = styled.div`
     min-height: 100dvh;
+    overflow-x: hidden;
+    background-color: ${props => props.theme.background};
+    padding-bottom: 1.2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const FullPageWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-between;
-    overflow-x: hidden;
-    background-color: ${props => props.theme.background};
-    padding-bottom: 1.2rem;
+    max-width: 1800px;
 `;
 
 const PageContent = styled.div`
